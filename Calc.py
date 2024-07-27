0647f2374c41dbdd80ad13ca3eaaf629ace4ac99
namanSeth-creator <76241731+namanSeth-creator@users.noreply.github.com>
2024-07-27 10:18:18 +0530

    calc file update

--- Calc.py
+++ Calc.py
@@ -1,4 +1,5 @@
-
+import Subtraction
+import Multiplication
 
 print("Select operation.")
 print("1. Add")
@@ -6,3 +7,28 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 print("5. Power")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        if choice == '2':
+            print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
